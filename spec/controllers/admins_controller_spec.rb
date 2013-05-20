c5968f82a8e30d9b1540d5f99ffb75d0372cd2b1
erlanggat <etjhie@thoughtworks.com>
2013-05-20 22:14:22 +1000

    test more authentication for admins controller

--- spec/controllers/admins_controller_spec.rb
+++ spec/controllers/admins_controller_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 describe AdminsController do
-  it "should login" do
-    username  = "username"
-    password = "password"
-    User.stub(:authenticate).with(username, password).and_return(1)  
-    post :login, { username: username, password: password }
-    session[:user_id].should == 1
+  context "Authenticate Successfully" do
+    before(:each) do 
+      username  = "username"
+      password = "password"
+      User.stub(:authenticate).with(username, password).and_return(1)  
+      post :login, { username: username, password: password }
+    end
+
+    it "should set session with user id" do
+      session[:user_id].should == 1
+    end
+
+    it "should display show_all view" do
+      response.should render_template(:shosw_all)
+    end
   end
 end
