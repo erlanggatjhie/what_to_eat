dee91146d977f9cbbdb82e3ace3342663e3a7e3f
erlangga tjhie <erlangga_tjhie@hotmail.com>
2013-05-28 21:29:20 +1000

    Fixing issue where restaurant object is displayed on the screen

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Restaurant.create(name: "Restaurant bitches", location: "Location bitches")
